45d6e5be48ac1ec2b262aab0eb4f71844169f320
Jane Castleman <castlemanje@gmail.com>
2022-07-08 12:47:08 -0400

    debug

--- index.py
+++ index.py
@@ -1,22 +1,10 @@
-from doctest import DocTestRunner
-from math import perm
-from os import remove, environ
-# import environ as envi
-from os import remove
-import sched
-from time import time
-from unicodedata import name
-from urllib import response
-from xml.dom import domreg
+
 from flask import Flask, request, make_response, redirect, url_for
 from flask import render_template, session
-from html import escape  # Used to thwart XSS attacks.
-from cgi import FieldStorage
-from sys import audit, stderr
-from urllib.parse import unquote
 
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='.')
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
