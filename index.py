bcb34b11424ebbc21bfe9ae8b7f688abe3146389
Jane Castleman <castlemanje@gmail.com>
2022-07-08 12:40:42 -0400

    removing errorneous imports

--- index.py
+++ index.py
@@ -1,23 +1,27 @@
+from doctest import DocTestRunner
+from math import perm
+from os import remove, environ
+# import environ as envi
+from os import remove
+import sched
+from time import time
+from unicodedata import name
+from urllib import response
+from xml.dom import domreg
 from flask import Flask, request, make_response, redirect, url_for
 from flask import render_template, session
+from html import escape  # Used to thwart XSS attacks.
+from cgi import FieldStorage
+from sys import audit, stderr
+from urllib.parse import unquote
 
 
 app = Flask(__name__)
-
-app.config.update(
-    SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE='Lax'
-)
-
-import auth
-
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
     html = render_template('index.html')
     response = make_response(html)
-    #clears any existing cookies, need to add back later
-    #response.set_cookie('netID', '', max_age=0)
     return response
+
