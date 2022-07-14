a98db03bdf9acd6b763cac0170b01da84ac077be
Jane Castleman <castlemanje@gmail.com>
2022-07-14 11:08:39 -0400

    adding filler vars and radio button functionality

--- card.py
+++ card.py
@@ -1,12 +1,13 @@
 class Card:
 
     def __init__(self, k, temp, precip, 
-    slope, wtd):
+    slope, wtd, num):
         self._k = k
         self._temp = temp
         self._precip = precip
         self._slope = slope
         self._wtd = wtd
+        self._num = num
         # self._group is only set from the auditionee page (where it is
         # needed)
 
@@ -17,6 +18,7 @@ class Card:
         s += " " + str(self._precip)
         s += " " + str(self._slope)
         s += " " + str(self._wtd)
+        s += " " + str(self._num)
         return s
 
     def to_dict(self):
@@ -31,6 +33,7 @@ class Card:
         d['precip'] = self._precip
         d['slope'] = self._slope
         d['wtd'] = self._wtd
+        d['num'] = self._num
 
         return d
 
@@ -53,6 +56,10 @@ class Card:
     def get_wtd(self):
 
         return self._wtd
+    
+    def get_num(self):
+
+        return self._num
 
     def get_color(self, var):
         if (var == "Low"):
--- index.py
+++ index.py
@@ -16,9 +16,18 @@ def index():
 
 @app.route('/cards', methods=['GET', 'POST'])
 def card_page():
-    num_cards = request.args.get('num_cards')
-    cards = get_cards(num_cards)
-    html = render_template('cards.html', cards=cards, num_cards=num_cards)
+    num_train_cards = request.args.get('num_train_cards')
+    num_test_cards = request.args.get('num_test_cards')
+    var_type = request.args.get('variable_type')
+
+    train_cards, test_cards = get_cards_2(num_train_cards, 
+    num_test_cards)
+
+    if (var_type == "wtd_vars"): card_html = 'cards.html'
+    else: card_html = 'fakecards.html'
+    
+    html = render_template(card_html, train_cards=train_cards, 
+                            test_cards=test_cards)
     response = make_response(html)
     return response
 
@@ -33,8 +42,8 @@ def get_value(dist):
         return "High"
 
 
-def get_cards(num_cards):
-    if (num_cards is None or num_cards is ""): return "Oh no!"
+def get_cards(num_cards, isTrain):
+    if (num_cards is None or num_cards == ""): print("Oh no!")
     cards = []
     n_cards = int(num_cards)
 
@@ -56,6 +65,7 @@ def get_cards(num_cards):
             P = get_value(P_low)
             topo = get_value(topo_low)
             wtd = "Low"
+            if (not isTrain): wtd = ""
             current_card = Card(k, ET, P, topo, wtd)
             
         else:
@@ -65,6 +75,7 @@ def get_cards(num_cards):
             P = get_value(P_high)
             topo = get_value(topo_high)
             wtd = "High"
+            if (not isTrain): wtd = ""
             current_card = Card(k, ET, P, topo, wtd)
 
         cards.append(current_card)
@@ -73,5 +84,68 @@ def get_cards(num_cards):
     return cards
 
 
+def get_low():
+    val = np.random.uniform(0, 0.63)
+    val = int(val * 100) // 29
+    if (val == 0): return "Low"
+    elif (val == 1): return "Medium"
+    else: return "High"
+
+def get_high():
+    val = np.random.uniform(0.37, 1)
+    val = int((val * 100)-15) // 29
+    if (val == 0): return "Low"
+    elif (val == 1): return "Medium"
+    else: return "High"
+
+def low():
+    val = (np.random.uniform(0,1) * 100) // 40
+    if (val == 0): return "Medium"
+    else: return "Low"
+
+def high():
+    val =  (np.random.uniform(0,1) * 100) // 40
+    if (val == 0): return "Medium"
+    else: return "High"
+
+# def __init__(self, k, temp, precip, slope, wtd)
+def get_cards_2(num_train_cards, num_test_cards):
+    train_cards = []
+    test_cards = []
+
+    for i in range(int(num_train_cards)) :
+        if (np.random.rand() < 0.5):
+            wtd = "Low"
+            current_card = Card(low(), high(), low(), high(), wtd, i + 1)
+        else:
+            wtd = "High"
+            current_card = Card(high(), low(), high(), low(), wtd, i + 1)
+
+        train_cards.append(current_card)
+
+    for i in range(int(num_test_cards)) :
+        if (np.random.rand() < 0.5):
+            wtd = "Low"
+            test_card = Card(low(), high(), low(), high(), "", i + 1)
+            key_card = Card(test_card.get_k(), test_card.get_temp(), 
+            test_card.get_precip(), test_card.get_slope(), wtd, i + 1)
+
+
+        else:
+            wtd = "High"
+            test_card = Card(high(), low(), high(), low(), "", i + 1)
+            key_card = Card(test_card.get_k(), test_card.get_temp(), 
+            test_card.get_precip(), test_card.get_slope(), wtd, i + 1)
+
+
+        test_cards.append(test_card)
+        test_cards.append(key_card)
+
+    return train_cards, test_cards
+
+
 def main():
-    pass
+    cards = get_cards_2(5, True)
+
+
+# main()
