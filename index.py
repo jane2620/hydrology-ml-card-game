65d2d6f65a69e8b2823dfb0c6d0a9af7238ae150
Jane Castleman <castlemanje@gmail.com>
2022-07-14 13:39:05 -0400

    separating testing set keys

--- index.py
+++ index.py
@@ -20,68 +20,28 @@ def card_page():
     num_test_cards = request.args.get('num_test_cards')
     var_type = request.args.get('variable_type')
 
-    train_cards, test_cards = get_cards_2(num_train_cards, 
+    train_cards, test_cards, key_cards = get_cards(num_train_cards, 
     num_test_cards)
 
     if (var_type == "wtd_vars"): card_html = 'cards.html'
     else: card_html = 'fakecards.html'
-    
+
     html = render_template(card_html, train_cards=train_cards, 
-                            test_cards=test_cards)
+                            test_cards=test_cards, 
+                            key_cards = key_cards)
     response = make_response(html)
     return response
 
+@app.route('/answers', methods=['GET', 'POST'])
+def answer_page():
+    var_type = request.args.get('variable_type')
 
-def get_value(dist):
-    out = np.random.rand()
-    if (out < dist[0]):
-        return "Low"
-    elif (out < dist[1]):
-        return "Medium"
-    else:
-        return "High"
+    if (var_type == "wtd_vars"): card_html = 'cards.html'
+    else: card_html = 'fakecards.html'
 
-
-def get_cards(num_cards, isTrain):
-    if (num_cards is None or num_cards == ""): print("Oh no!")
-    cards = []
-    n_cards = int(num_cards)
-
-
-    k_high = [.2, .5, 1]
-    k_low = [.5, .8, 1]
-    ET_low = [.2, .5, 1]
-    ET_high = [.5, .8, 1]
-    P_high = [.2, .5, 1]
-    P_low = [.5, .8, 1]
-    topo_high = [.2, .5, 1]
-    topo_low = [.5, .8, 1]
-
-    for i in range(n_cards):
-        if (np.random.rand() < 0.5):
-            # low WTD
-            k = get_value(k_low)
-            ET = get_value(ET_low)
-            P = get_value(P_low)
-            topo = get_value(topo_low)
-            wtd = "Low"
-            if (not isTrain): wtd = ""
-            current_card = Card(k, ET, P, topo, wtd)
-            
-        else:
-            # high WTD
-            k = get_value(k_high)
-            ET = get_value(ET_high)
-            P = get_value(P_high)
-            topo = get_value(topo_high)
-            wtd = "High"
-            if (not isTrain): wtd = ""
-            current_card = Card(k, ET, P, topo, wtd)
-
-        cards.append(current_card)
-
-
-    return cards
+    html = render_template(card_html, answers=key_cards)
+    response = make_response(html)
+    return response
 
 
 def get_low():
@@ -109,9 +69,10 @@ def high():
     else: return "High"
 
 # def __init__(self, k, temp, precip, slope, wtd)
-def get_cards_2(num_train_cards, num_test_cards):
+def get_cards(num_train_cards, num_test_cards):
     train_cards = []
     test_cards = []
+    key_cards = []
 
     for i in range(int(num_train_cards)) :
         if (np.random.rand() < 0.5):
@@ -139,13 +100,13 @@ def get_cards_2(num_train_cards, num_test_cards):
 
 
         test_cards.append(test_card)
-        test_cards.append(key_card)
+        key_cards.append(key_card)
 
-    return train_cards, test_cards
+    return train_cards, test_cards, key_cards
 
 
 def main():
-    cards = get_cards_2(5, True)
+    cards = get_cards(5, True)
 
 
 # main()
